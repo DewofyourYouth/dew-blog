3a9b569d05353b0594067cfe6f6be73ec4c9c4ba
DewofyourYouth <jacobshore@gmail.com>
2025-08-17 19:25:30 +0300

    fix cloze

--- assets/js/site.js
+++ assets/js/site.js
@@ -4,3 +4,20 @@ document.addEventListener('mouseover', e => {
   e.target.animate([{ transform:'scale(1)' },{ transform:'scale(1.06)' },{ transform:'scale(1)' }],
                    { duration:160, easing:'ease-out' });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let clozes = document.querySelectorAll("li.cloze");
+  for (let cloze of clozes) {
+    let button = document.createElement("button")
+    button.innerText = "👁️"
+    button.classList.add("toggle-btn")
+    cloze.appendChild(button)
+    button.addEventListener("click", (ev) => {
+      ev.target.parentElement.classList.toggle("reveal");
+    })
+    cloze.addEventListener("click", (ev) => {
+      ev.stopPropagation(); // don't bubble to li
+      ev.target.classList.toggle("reveal");
+    });
+  }
+});
